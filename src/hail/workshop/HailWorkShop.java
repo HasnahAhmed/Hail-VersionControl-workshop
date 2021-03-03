fe4adf9c6f3de9d2b7abd142bcf3ca51e5f185d2
HasnahAhmed <79948246+HasnahAhmed@users.noreply.github.com>
2021-03-03 03:41:17 +0300

    code java
    code java

---
+++ src/hail/workshop/HailWorkShop.java
@@ -0,0 +1,14 @@
+
+package hail.workshop;
+
+
+public class HailWorkShop {
+
+    
+    public static void main(String[] args) {
+        System.out.println("This is first Virsion Control");
+        System.out.println("Thank you doctor");
+
+    }
+    
+}
